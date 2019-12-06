377a932d8d8354cf07ebfa70f0f202b599d6373b
Jonah Alligood <jonahisadev@gmail.com>
2019-12-05 23:13:37 -0600

    Day 2 - Part 1

---
+++ day2/main.cpp
@@ -0,0 +1,66 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+enum Opcode
+{
+    ADD = 1,
+    MUL = 2,
+    HLT = 99
+};
+
+void read_inst(std::vector<int>& inst)
+{
+    std::string buffer;
+    
+    std::ifstream file("input.txt");
+    std::getline(file, buffer);
+    file.close();
+
+    size_t pos = 0;
+    while ((pos = buffer.find(',')) != std::string::npos) {
+        std::string token = buffer.substr(0, pos);
+        inst.push_back(std::stoi(token));
+        buffer.erase(0, pos + 1);
+    }
+}
+
+int main()
+{
+    std::vector<int> inst;
+    read_inst(inst);
+
+    int pc = 0;
+
+    // Loop
+    while (inst.at(pc) != Opcode::HLT) {
+        int x = inst.at(inst.at(pc + 1));
+        int y = inst.at(inst.at(pc + 2));
+        int loc = inst.at(pc + 3);
+
+        int opcode = inst.at(pc);
+        switch (opcode) {
+            case Opcode::ADD: {
+                inst[loc] = x + y;
+                break;
+            }
+
+            case Opcode::MUL: {
+                inst[loc] = x * y;
+                break;
+            }
+
+            default: {
+                std::cout << "Error at PC " << pc << std::endl;
+                break;
+            }
+        }
+
+        pc += 4;
+    }
+
+    std::cout << "Final value: " << inst.at(0) << std::endl;
+
+    return 0;
+}
